d862713ad4b022b598d9c8e8e565ed41ed3bc626
wisnubayuaji <wisnubayu34@gmail.com>
2020-10-20 19:43:01 +0700

    Add files via upload

---
+++ if2-10116430-Latihan9-Teks/src/App.java
@@ -0,0 +1,15 @@
+/**
+ * @author
+ * Nim  : 10116430
+ * Nama : Wisnu Bayu Aji
+ * Kelas: IF-2
+ */
+public class App {
+    public static void main(String[] args) throws Exception {
+        char c;
+        char tab = '\t';
+        char newline = '\n';
+        c = 'A';
+        System.out.println("Hasil" + newline + "Char: " + tab + "berisi " + c);
+    }
+}
